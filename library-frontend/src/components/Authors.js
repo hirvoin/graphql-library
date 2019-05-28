2ecab2915b2c7ecae859b1d064b8b479b0b57dae
Aatu Hirvonen <aatu95@gmail.com>
2019-05-28 13:50:46 +0200

    books listing

--- library-frontend/src/components/Authors.js
+++ library-frontend/src/components/Authors.js
@@ -49,29 +49,6 @@ const Authors = props => {
       </Query>
     </div>
   )
-
-  // {result.data.allAuthors.map(author => console.log(author))}
-  // return (
-  //   <div>
-  //     <h2>authors</h2>
-  //     <table>
-  //       <tbody>
-  //         <tr>
-  //           <th />
-  //           <th>born</th>
-  //           <th>books</th>
-  //         </tr>
-  //         {authors.map(a => (
-  //           <tr key={a.name}>
-  //             <td>{a.name}</td>
-  //             <td>{a.born}</td>
-  //             <td>{a.bookCount}</td>
-  //           </tr>
-  //         ))}
-  //       </tbody>
-  //     </table>
-  //   </div>
-  // )
 }
 
 export default Authors
--- library-frontend/src/components/Books.js
+++ library-frontend/src/components/Books.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React from "react"
+import { Query } from "react-apollo"
+import { gql } from "apollo-boost"
 
-const Books = (props) => {
+const ALL_BOOKS = gql`
+  {
+    allBooks {
+      title
+      author
+      published
+    }
+  }
+`
+
+const Books = props => {
   if (!props.show) {
     return null
   }
@@ -9,28 +21,32 @@ const Books = (props) => {
 
   return (
     <div>
-      <h2>books</h2>
-
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>
-              author
-            </th>
-            <th>
-              published
-            </th>
-          </tr>
-          {books.map(a =>
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author}</td>
-              <td>{a.published}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+      <h2>Books</h2>
+      <Query query={ALL_BOOKS}>
+        {result => {
+          if (result.loading) {
+            return <div>loading...</div>
+          }
+          return (
+            <table>
+              <tbody>
+                <tr>
+                  <th />
+                  <th>author</th>
+                  <th>published</th>
+                </tr>
+                {result.data.allBooks.map(a => (
+                  <tr key={a.title}>
+                    <td>{a.title}</td>
+                    <td>{a.author}</td>
+                    <td>{a.published}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )
+        }}
+      </Query>
     </div>
   )
 }
