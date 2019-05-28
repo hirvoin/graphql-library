db9f55c1eb843cf1189a4474abd4a87083af8702
Aatu Hirvonen <aatu95@gmail.com>
2019-05-28 13:31:04 +0200

    kirjailijoiden näkymä

--- library-frontend/src/components/Authors.js
+++ library-frontend/src/components/Authors.js
@@ -1,6 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState } from "react"
+import { Query } from "react-apollo"
+import { gql } from "apollo-boost"
 
-const Authors = (props) => {
+const ALL_AUTHORS = gql`
+  {
+    allAuthors {
+      name
+      born
+      bookCount
+    }
+  }
+`
+
+const Authors = props => {
   if (!props.show) {
     return null
   }
@@ -8,30 +20,58 @@ const Authors = (props) => {
 
   return (
     <div>
-      <h2>authors</h2>
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>
-              born
-            </th>
-            <th>
-              books
-            </th>
-          </tr>
-          {authors.map(a =>
-            <tr key={a.name}>
-              <td>{a.name}</td>
-              <td>{a.born}</td>
-              <td>{a.bookCount}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+      <h2>Authors</h2>
+      <Query query={ALL_AUTHORS}>
+        {result => {
+          if (result.loading) {
+            return <div>loading...</div>
+          }
 
+          return (
+            <table>
+              <tbody>
+                <tr>
+                  <th />
+                  <th>Born</th>
+                  <th>Books</th>
+                </tr>
+                {result.data.allAuthors.map(a => (
+                  <tr key={a.name}>
+                    <td>{a.name}</td>
+                    <td>{a.born}</td>
+                    <td>{a.bookCount}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )
+        }}
+      </Query>
     </div>
   )
+
+  // {result.data.allAuthors.map(author => console.log(author))}
+  // return (
+  //   <div>
+  //     <h2>authors</h2>
+  //     <table>
+  //       <tbody>
+  //         <tr>
+  //           <th />
+  //           <th>born</th>
+  //           <th>books</th>
+  //         </tr>
+  //         {authors.map(a => (
+  //           <tr key={a.name}>
+  //             <td>{a.name}</td>
+  //             <td>{a.born}</td>
+  //             <td>{a.bookCount}</td>
+  //           </tr>
+  //         ))}
+  //       </tbody>
+  //     </table>
+  //   </div>
+  // )
 }
 
 export default Authors
